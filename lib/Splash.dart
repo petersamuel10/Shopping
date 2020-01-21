a1aa4aa1e68468f6bcb62e188587c900c5c858f1
petersamuel10 <petersamuelyakoub@gmail.com>
2020-01-21 17:23:58 +0300

    splash screen

--- lib/Splash.dart
+++ lib/Splash.dart
@@ -12,25 +12,73 @@ class _SplashState extends State<Splash> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer(Duration(seconds: 5), () => Navigator.pushNamed(context, '/home'));
+     Timer(Duration(seconds: 5), () => Navigator.pushNamed(context, '/home'));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/logo.jpg',
-              height: 160,
-              width: 160,
-            ),
-          ],
-        ),
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.pink),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 6,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 70.0,
+                        child: Image.asset(
+                          'assets/logo.jpg',
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        'Dukkan Express',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(padding: EdgeInsets.only(top: 20.0)),
+                    Text(
+                      'Online Store\n For Everyone',
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12.0,
+                          color: Colors.white),
+                    ),
+                    Padding(padding: EdgeInsets.only(bottom: 12.0)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
